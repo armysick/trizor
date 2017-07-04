1cd6b01a19003a82a93c8dc021cb2543cffc60c4
armysick <ahsdfasdfh>
2017-07-04 19:01:00 +0100

    There ya go

---
+++ r/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace r
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form f2 = new Form2();
+            if (textBox1.Text.ToUpper() == "TRIZOR" && textBox2.Text == "pokemon")
+            {
+                f2.Show();
+                this.Hide();
+            }
+
+        }
+    }
+}
